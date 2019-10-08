7b4c58d369bb6de76034cd038546fa07695a7b08
Antonio-PC\antonioDataScience <atolic@racunarstvo.hr>
2019-10-08 13:32:33 +0200

    initial

---
+++ Intro.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+import os
+
+def main():
+    args = sys.argv[1:]
+    print("Taking arg...")
+    for i, arg in enumerate(args):
+        print(i,arg)
+
+if __name__ == '__main__':
+    main()
